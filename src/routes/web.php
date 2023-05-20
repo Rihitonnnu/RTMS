2f8edf4e3d73553b23f5cb0f9d68fb14da81b1fc
Rihitonnnu <hexiliang04@gmail.com>
2023-05-20 16:29:47 +0900

    fix: ログイン画面修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,11 +17,8 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+    return Inertia::render('Auth/Login', [
+        'canResetPassword' => true
     ]);
 });
 
@@ -35,4 +32,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
