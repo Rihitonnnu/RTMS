1377e4c10e1fae55346ceda84ccefbf4d64b4773
Rihitonnnu <hexiliang04@gmail.com>
2023-05-28 18:15:38 +0900

    feat: ヘッダー追加

--- resources/js/Pages/TimeManagement/TargetTimeEdit.tsx
+++ resources/js/Pages/TimeManagement/TargetTimeEdit.tsx
@@ -8,6 +8,7 @@ import type {
   TargetTimeEditProps,
   TargetTimeInputs
 } from '@/types/TimeManagement/TimeManagementType';
+import Layout from '@/Layouts/Layout';
 
 function TargetTimeEdit({ targetTime }: TargetTimeEditProps) {
   const schema = yup.object({
@@ -31,42 +32,44 @@ function TargetTimeEdit({ targetTime }: TargetTimeEditProps) {
     router.put(route('targetTime.update', targetTime.id), data);
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex items-center w-fit mt-4 mx-auto">
-        <Controller
-          name="time"
-          control={control}
-          render={({ field }) => (
-            <TextField
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              {...field}
-              defaultValue={targetTime.time}
-              id="outlined-number"
-              label="週間目標時間（時間）"
-              type="number"
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              {...register('time')}
-              error={'time' in errors}
-              helperText={errors.time?.message}
-            />
-          )}
-        />
-        <div className="ml-3">
-          <Button type="submit" variant="contained" color="primary">
-            設定する
-          </Button>
+    <Layout>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex items-center w-fit mt-4 mx-auto">
+          <Controller
+            name="time"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...field}
+                defaultValue={targetTime.time}
+                id="outlined-number"
+                label="週間目標時間（時間）"
+                type="number"
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...register('time')}
+                error={'time' in errors}
+                helperText={errors.time?.message}
+              />
+            )}
+          />
+          <div className="ml-3">
+            <Button type="submit" variant="contained" color="primary">
+              設定する
+            </Button>
+          </div>
+          <div className="ml-3">
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => router.get(route('dashboard'))}
+            >
+              戻る
+            </Button>
+          </div>
         </div>
-        <div className="ml-3">
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => router.get(route('dashboard'))}
-          >
-            戻る
-          </Button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </Layout>
   );
 }
 
