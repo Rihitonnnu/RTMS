6dd97d4b0b7f14c93b727736ec204c706f22faef
Rihitonnnu <hexiliang04@gmail.com>
2023-05-29 14:58:39 +0900

    fix: interfaceで型定義

--- app/Repositories/DailyTime/DailyTimeRepositoryInterface.php
+++ app/Repositories/DailyTime/DailyTimeRepositoryInterface.php
@@ -10,6 +10,7 @@ interface DailyTimeRepositoryInterface
     public function updateResearchTime(DailyTime $dailyTime, float $researchTime): bool;
     public function storeRestTime(float $restTime): void;
     public function updateRestTime(DailyTime $dailyTime, float $restTime): bool;
-    public function updateDailyTime($researchTime, $restTime, DailyTime $dailyTime);
+    public function updateDailyTime(float $researchTime, float $restTime, DailyTime $dailyTime);
     public function destroyDailyTime(DailyTime $dailyTime);
+    public function storeNotRegisteredDay($firstDayThisMonth, $now);
 }
