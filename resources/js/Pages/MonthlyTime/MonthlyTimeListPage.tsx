b26b53bb2f73d923ed1103e584e10c74c9bf3c50
Rihitonnnu <hexiliang04@gmail.com>
2023-05-29 16:03:01 +0900

    feat: フラッシュメッセージを追加

--- resources/js/Pages/MonthlyTime/MonthlyTimeListPage.tsx
+++ resources/js/Pages/MonthlyTime/MonthlyTimeListPage.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+
+import Flash from '@/Layouts/Flash';
 import Layout from '@/Layouts/Layout';
 import type { MonthlyTimeListPageProps } from '@/types/MonthlyTime/MonthlyTimeType';
 import MonthyTimeListTable from './MonthyTimeListTable';
@@ -6,9 +9,19 @@ function MonthlyTimeListPage({
   thisMonthInfos,
   thisMonthResearchTime
 }: MonthlyTimeListPageProps) {
+  const [open, setOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    setOpen(true);
+    setTimeout(() => {
+      setOpen(false);
+    }, 2000);
+  }, [thisMonthInfos]);
+
   return (
     <Layout>
       <div className="max-w-7xl w-5/6 mx-auto mt-10 sm:px-6 lg:px-8 justify-evenly items-center">
+        <Flash open={open} />
         <div className="flex items-center mb-3">
           <h1 className="font-bold text-2xl">今月の研究時間</h1>
           <h1 className="ml-3 font-bold text-2xl">{`${thisMonthResearchTime}時間`}</h1>
