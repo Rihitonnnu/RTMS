e94243c35d2794b553993f7367122dc200f9485f
Rihitonnnu <hexiliang04@gmail.com>
2023-05-29 14:17:23 +0900

    fix: リレーションの定義漏れを修正

--- app/Models/Research.php
+++ app/Models/Research.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Research extends Model
 {
@@ -26,5 +27,10 @@ class Research extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function rests(): HasMany
+    {
+        return $this->hasMany(Rest::class);
     }
 }
